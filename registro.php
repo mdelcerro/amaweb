d834fd7f4a10681333dbc86d207bed732731a984
aoleagagoitia <aoleagagoitia@fp.uoc.edu>
2018-05-02 17:46:14 +0200

    registro

---
+++ registro.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="es">
+<head>
+<meta charset="utf-8">
+<title>Registro y login</title>
+</head>
+
+<body>
+	<form action="" method="post">
+		<label>Nombre de usuario: </label><input type="text" name="userreg"><br /><br />
+		<label>Contraseña: </label><input type="password" name="pwreg"><br /><br />
+		<input type="submit" name="registrar" value="Registrarme">
+	</form>
+</body>
+</html>
