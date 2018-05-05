0688cd8b841a6bbe24782491ee7782891ff9536b
Melissa del Cerro <mdelcerrop@fp.uoc.edu>
2018-05-05 19:42:24 +0200

    Add utils file for database connection and sessions

---
+++ utils.php
@@ -0,0 +1,50 @@
+<?php
+
+function connect() {
+
+    $servername = "localhost";
+    $username = "id5508088_amaweb";
+    $password = "123456";
+    $schema = "id5508088_amaweb";
+
+    /*
+    //datos base de datos local
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $schema = "INPRL";
+    */
+
+    // Conectar DB
+    $conn = new mysqli($servername, $username, $password);
+    mysqli_set_charset($conn,"utf8");
+
+    // Comprobar conexión
+    if ($conn->connect_error) {
+        die("<p style=\"color:red\">Fallo de conexión: </p><br>" . $conn->connect_error);
+    }
+    // Seleccionar bd
+    $sql = "USE ".$schema;
+    if ($conn->query($sql) === FALSE) {
+        die("<br><p style=\"color:red\">La BBDD ".$schema." no pudo ser seleccionada: </p><br>" . $conn->error);
+    }
+
+    return $conn;
+}
+
+function is_logged() {
+    return !empty($_SESSION['dni']);
+}
+
+function validate_security() {
+
+    if (!is_logged()) {
+        header('Location: acceso.php');
+        die();
+    }
+}
+
+function get_logged() {
+    return $_SESSION['dni'];
+}
