689112e0b56988758d53295a93dac8e4f2057e0c
aoleagagoitia <aoleagagoitia@fp.uoc.edu>
2018-05-12 16:17:05 +0200

    envio variables

--- modificarTablaEnt.php
+++ modificarTablaEnt.php
@@ -2,14 +2,14 @@
 session_start();
 include "includes/conexion.php";
 $titu=$_GET['recogiendo0'];
-$usua=$_GET['recogiendo1'];
-$enlace=$_GET['recogiendo2'];
-$fech=$_GET['recogiendo3'];
+
+$enlace=$_GET['recogiendo1'];
+$fech=$_GET['recogiendo2'];
 ?>
 	<h1>Modificar datos de la noticia </h1>
 	<form name="formResultado" action="r_modificarTablaEnt.php" method="post">
         <input name="d_titu" type="text" value="<?php echo $titu;?>">
-    	<input name="d_usua" type="text" readonly value="<?php echo $usua;?>">
+   
  		<input name="d_enlace" type="text"  value="<?php echo $enlace;?>">
         <input name="d_fech" type="text" readonly value="<?php echo $fech;?>">
 	<input name="btnmodificar" type="submit" value="Modificar">        
