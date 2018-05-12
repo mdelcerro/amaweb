89441473a8c3f9b3f58545f5925b46109507d38a
aoleagagoitia <aoleagagoitia@fp.uoc.edu>
2018-05-12 16:20:53 +0200

    cambio login

--- r_comboEntradas.php
+++ r_comboEntradas.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include "includes/conexion.php";
 ?>
 <!doctype html>
 <html>
@@ -9,13 +10,13 @@ session_start();
 </head>
 <body>
 <?php
-include "includes/conexion.php";
+
 $tit = $_POST['entradas'];
-$sql_select = "SELECT titulo, usuario, enlace, fecha from rss where titulo='$tit'";
+$sql_select = "SELECT titulo, enlace, fecha from rss where titulo='$tit'";
 $eje_select = mysqli_query($conexion, $sql_select) or die ("No se puede realizar la conexion");
 ?>
 <table id='tabla' width="800" border="1">
-<th>Titulo</th><th>Usuario</th><th>Enlace</th><th>Fecha publicación</th><th>BORRAR</th><th>MODIFICAR</th>
+<th>Titulo</th><th>Enlace</th><th>Fecha publicación</th><th>BORRAR</th><th>MODIFICAR</th>
  <?php      
 		while($rgto = mysqli_fetch_row($eje_select))
 		{
@@ -24,10 +25,10 @@ $eje_select = mysqli_query($conexion, $sql_select) or die ("No se puede realizar
          	<td><?php echo $rgto[0];?></td>
          	<td><?php echo $rgto[1];?></td>
             <td><?php echo $rgto[2];?></td>
-             <td><?php echo $rgto[3];?></td>
+            
          	
             <td><a href="borrarTablaEnt.php?recogiendo0=<?php echo $rgto[0] ?>">BORRAR</a></td>
-            <td><a href="modificarTablaEnt.php?recogiendo0=<?php echo $rgto[0] ?>&recogiendo1=<?php echo $rgto[1] ?>&recogiendo2=<?php echo $rgto[2] ?>&recogiendo3=<?php echo $rgto[3] ?>">MODIFICAR</a></td>
+            <td><a href="modificarTablaEnt.php?recogiendo0=<?php echo $rgto[0] ?>&recogiendo1=<?php echo $rgto[1] ?>&recogiendo2=<?php echo $rgto[2] ?>">MODIFICAR</a></td>
     	 </tr>
 		 <?php 
 		}
