e1df559c4b134b644f6fef264c1552f9b7bed4a3
aoleagagoitia <aoleagagoitia@fp.uoc.edu>
2018-05-12 16:21:21 +0200

    cambio conexion

--- r_formInsertaNews.php
+++ r_formInsertaNews.php
@@ -1,12 +1,16 @@
 <?php
 session_start();
-
-if (isset($_SESSION['usuario'])){
-    $v_usuario = $_SESSION['usuario'];
+include "includes/conexion.php";
+if (isset($_SESSION['user'])){
+    $v_usuario = $_SESSION['user'];
 }
 
 
-include "includes/conexion.php";
+$sql="SELECT idusuario from usuarios where usuario='$v_usuario'";
+$exe=mysqli_query($conexion, $sql);
+$res=mysqli_fetch_array($exe);
+$id= $res[0];
+
 //recojo datos
 $v_titulo = $_POST["titulo"];// Post del input name
 //$v_usuario = $_POST["usuario"];
@@ -14,20 +18,23 @@ $v_enlace = $_POST["enlace"];
 $v_fecha = date("Y/m/d");
 
 
-
-
 //primero hay que comprobar si existe ya
 $sql_select = "select titulo from rss where titulo='$v_titulo'";
 $eje_select = mysqli_query($conexion, $sql_select) or die("no puede realizar la selección");
 $contador = mysqli_num_rows($eje_select);
 
+
+
+
+
+
 if (empty($v_titulo) )//Esta comprobación no sería necesaria al poner required en el formulario
 {echo "Error. Debe rellenar todos los campos";}
 
 if ($contador==0){
-	$sql_alta = "insert into rss (titulo, fecha, usuario, enlace) values ('$v_titulo', '$v_fecha', '$v_usuario', '$v_enlace')";
-	
-	$ejecuto_alta = mysqli_query($conexion, $sql_alta);//hago el insert porque no hay registro igual. 
+    $sql_alta = "insert into rss (titulo, fecha, idusuario, enlace) values ('$v_titulo', '$v_fecha', '$id', '$v_enlace')";
+
+    $ejecuto_alta = mysqli_query($conexion, $sql_alta);//hago el insert porque no hay registro igual.
 
 }
 
@@ -36,20 +43,23 @@ if ($contador==0){
 ?>
 
 <table style="width:800px" >
-	<th>TITULO</th>
-	<th>USUARIO</th>
+    <th>TITULO</th>
+    <th>USUARIO</th>
     <th>ENLACE</th>
-	<th>FECHA</th>
-	   
-    
+    <th>FECHA</th>
+
+
     <tr>
-    	
-	  	<td><?php echo $v_titulo;?></td>
-	  	<td><?php echo $v_usuario;?></td> 
-	    <td><?php echo $v_enlace;?></td> 
-	  	<td><?php echo $v_fecha;?></td>      
-	                 
-    </tr>       
+
+        <td><?php echo $v_titulo;?></td>
+        <td><?php echo $v_usuario;?></td>
+        <td><?php echo $v_enlace;?></td>
+        <td><?php echo $v_fecha;?></td>
+
+    </tr>
 </table>
 <br /><br />
 <a href="index.php">Volver al inicio</a>
+<br /><br />
+<a href="formInsertaNews.php">Inserta otra noticia</a>
+
