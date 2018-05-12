1d43fececb1029f35e04da99bd09555b9adad163
aoleagagoitia <aoleagagoitia@fp.uoc.edu>
2018-05-12 16:21:45 +0200

    cambio sql

--- r_modificarTablaEnt.php
+++ r_modificarTablaEnt.php
@@ -14,28 +14,25 @@ include "includes/conexion.php";
 //RECOJO LOS NUEVOS DATOS DEL FORMULARIO PARA EDITAR EL REGISTRO
 
 $ti=$_POST['d_titu'];
-$us=$_POST['d_usua'];
 $en=$_POST['d_enlace'];
 $fe=$_POST['d_fech'];
 
 //SENTENCIA UPDATE > Vamos a coger los datos nuevos y modificarlo en la base de datos
-$sql_update="UPDATE rss SET titulo='$ti', usuario='$us', enlace='$en' WHERE fecha='$fe'";
+$sql_update="UPDATE rss SET titulo='$ti',  enlace='$en' WHERE fecha='$fe'";
 
 //EJECUTAR EL UPDATE
 
 $ejec_update=mysqli_query($conexion, $sql_update) or die ("No se puede realizar el cambio.");
 
-echo "Registro actualizado<br/>";
+
 ?>
 <table border="1px">
 	<th>TITULO</th>
-	<th>USUARIO</th>
     <th>ENLACE</th>
 	<th>FECHA</th>
     
     <tr>
 		<td><?php echo $ti;?></td>
-    	<td><?php echo $us;?></td>
 	  	<td><?php echo $en;?></td>
         <td><?php echo $fe;?></td>
     </tr>       
