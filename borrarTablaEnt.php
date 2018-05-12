658e7ac89121f72a6708402ceb842cb9fe90fce7
aoleagagoitia <aoleagagoitia@fp.uoc.edu>
2018-05-12 16:11:04 +0200

    Cambios en mensajes

--- borrarTablaEnt.php
+++ borrarTablaEnt.php
@@ -6,6 +6,8 @@ include "includes/conexion.php";
 $borra = $_GET['recogiendo0'];
 $sql_baja = "delete from rss where titulo='$borra'";
 mysqli_query($conexion,$sql_baja);//hago el delete porque sabemos q el registro existe ya q viene de la tabla
-echo "Registro eliminado<br />";
-echo "<a href='combo_Entradas.php'>Seguir modificanco noticias</a>";
+echo "Noticia eliminada!<br />";
+echo "<a href='combo_Entradas.php'>Seguir modificanco noticias</a><br /><br />";
+
+echo "<a href='index.php'>Volver al inicio</a>";
 ?>
