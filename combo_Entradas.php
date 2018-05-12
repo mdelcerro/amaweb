ddd3ee46e164d6778b2e85a1f92eb7c826128af5
aoleagagoitia <aoleagagoitia@fp.uoc.edu>
2018-05-12 16:12:29 +0200

    Cambio en conexion

--- combo_Entradas.php
+++ combo_Entradas.php
@@ -1,18 +1,29 @@
 <?php
 session_start();
+include 'functions.php';
+include 'includes/conexion.php';
 ?>
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="es">
+
 <head>
-<meta charset="utf-8">
-<title>Modifica noticias</title>
+<title>Panel de Control</title>
+  <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+        <link rel="stylesheet" href="assets/css/main.css" />
+        <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
 </head>
+
 <body>
+    <?php  draw_header();?>  
+    
+<h2>Selecciona noticia para borrar o modificar</h2>
 <form action="r_comboEntradas.php" method="POST" name="form_entradas">
 <select name="entradas">
 <option value="" selected disabled>Elige una noticia</option>
 <?php
-include "includes/conexion.php";
+
 $sql_select_entradas = "SELECT DISTINCT titulo FROM rss order by fecha DESC";
 $eje_select_entradas = mysqli_query($conexion,$sql_select_entradas) or die ("No se puede hacer la selección");
 
